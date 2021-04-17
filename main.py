6601a332d5dc093ae09c1c29fe794a8c8fe3a731
AleksandrVlasov <c6282i@gmail.com>
2021-04-17 22:52:30 +0300

    test bot

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import telebot
+# import parser
+import config
 
+bot = telebot.TeleBot(config.token)
+
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет, я бот - парсер хабра.')
+    elif text == "как дела?":
+        bot.send_message(chat_id, 'Хорошо, а у тебя?')
+    else:
+        bot.send_message(chat_id, 'Простите, я вас не понял :(')
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
